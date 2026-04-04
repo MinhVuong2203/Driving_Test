aa638b14c0b56ae5aba89630f17e6a271227770c
MinhVuong2203 <vuonghihihihi@gmail.com>
2026-04-04 21:16:22 +0700

    connect firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
+import 'package:driving_test_prep/UserListPage.dart';
 import 'package:driving_test_prep/apps/app.dart';
+import 'package:driving_test_prep/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // runApp(
+  //   const MaterialApp(
+  //     home: UserListPage(),
+  //   ),
+  // );
+
   runApp(const MyApp());
+
 }
