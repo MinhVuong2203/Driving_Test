c6e38dfe60cdc7f4ef9f80feface9aaf4f2d8d62
MinhVuong2203 <vuonghihihihi@gmail.com>
2026-04-11 08:44:52 +0700

    Chia lại cấu trúc

--- lib/features/driving_centers/screens/center_list_screen.dart
+++ lib/features/driving_centers/screens/center_list_screen.dart
@@ -1,5 +1,5 @@
 
-import 'package:driving_test_prep/data/datasource/external/driving_center_service.dart';
+import 'package:driving_test_prep/data/datasource/external/service/driving_center_service.dart';
 import 'package:driving_test_prep/data/models/driving_center_model.dart';
 import 'package:driving_test_prep/features/driving_centers/screens/center_detail_screen.dart';
 
