278b6cbe5d624fcf51f3ac9cd688d4c7cf31cfb0
MinhVuong2203 <vuonghihihihi@gmail.com>
2026-05-20 17:46:56 +0700

    fix: update driving center feature

---
+++ lib/data/services/firebase/driving_center_api_service.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+
+import 'package:driving_test_prep/data/models/driving_center_model.dart';
+import 'package:driving_test_prep/shared/utils/constants/app_config.dart';
+import 'package:http/http.dart' as http;
+class DrivingCenterApiService {
+  static final String baseUrl = AppConfig.baseUrl;
+
+  Future<List<DrivingCenter>> fetchByProvince(String keyword) async {
+    final uri = Uri.parse(
+      '$baseUrl/api/DrivingCenters/search?keyword=${Uri.encodeComponent(keyword)}',
+    );
+
+    print('--------------- [ $uri ] ---------------');
+
+    final response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      final body = jsonDecode(response.body);
+
+      final List data = body['data'] ?? [];
+
+      return data
+          .map((item) => DrivingCenter.fromJson(item))
+          .toList();
+    }
+
+    throw Exception(
+      'Không thể tải danh sách trung tâm. Status: ${response.statusCode}',
+    );
+  }
+
+  Future<List<DrivingCenter>> fetchAll() async {
+    final uri = Uri.parse('$baseUrl/api/DrivingCenters');
+
+    final response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      final body = jsonDecode(response.body);
+
+      final List data = body['data'] ?? [];
+
+      return data
+          .map((item) => DrivingCenter.fromJson(item))
+          .toList();
+    }
+
+    throw Exception(
+      'Không thể tải danh sách trung tâm. Status: ${response.statusCode}',
+    );
+  }
+}
--- lib/shared/utils/constants/app_config.dart
+++ lib/shared/utils/constants/app_config.dart
@@ -1,4 +1,3 @@
 class AppConfig {
   static String get baseUrl => "http://10.0.2.2:7211";
-
 }
