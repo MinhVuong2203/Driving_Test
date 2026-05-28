847a1b081dc2dafaab334d5a80c9a62cf77c3047
sHunOrinn <161719997+sHunOrinn@users.noreply.github.com>
2026-05-28 14:09:00 +0700

    chore: update app configuration

--- lib/shared/utils/constants/app_config.dart
+++ lib/shared/utils/constants/app_config.dart
@@ -1,4 +1,4 @@
 class AppConfig {
-  static String get baseUrl => "http://10.0.2.2:7211";
-  // static String get baseUrl => "https://drivingtestadmin-production.up.railway.app";
+  // static String get baseUrl => "http://10.0.2.2:7211";
+  static String get baseUrl => "https://drivingtestadmin-production.up.railway.app";
 }
